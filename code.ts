8eb61fd4ab1fc2712deebf6c9278620797708cc7
ize-302 <adavizeozorku@gmail.com>
2023-11-17 23:56:27 +0100

    :sparkles: Add figjam support

--- code.ts
+++ code.ts
@@ -1,25 +1,23 @@
-if (figma.editorType === "figma") {
-  figma.showUI(__html__, { themeColors: true, height: 40, width: 200 });
-  figma.ui.onmessage = async (domain) => {
-    await fetch(`https://gravicon-plugin-server-pump.koyeb.app?url=${domain}`)
-      .then((response) => response.json())
-      .then((json) => {
-        figma.createImageAsync(json.data).then(async (image: Image) => {
-          // Create a rectangle that's the same dimensions as the image.
-          const node = figma.createRectangle();
-          const { width, height } = await image.getSizeAsync();
-          node.resize(width, height);
+figma.showUI(__html__, { themeColors: true, height: 40, width: 200 });
+figma.ui.onmessage = async (domain) => {
+  await fetch(`https://gravicon-plugin-server-pump.koyeb.app?url=${domain}`)
+    .then((response) => response.json())
+    .then((json) => {
+      figma.createImageAsync(json.data).then(async (image: Image) => {
+        // Create a rectangle that's the same dimensions as the image.
+        const node = figma.createRectangle();
+        const { width, height } = await image.getSizeAsync();
+        node.resize(width, height);
 
-          // Render the image by filling the rectangle.
-          node.fills = [
-            {
-              type: "IMAGE",
-              imageHash: image.hash,
-              scaleMode: "FILL",
-            },
-          ];
-          figma.closePlugin();
-        });
+        // Render the image by filling the rectangle.
+        node.fills = [
+          {
+            type: "IMAGE",
+            imageHash: image.hash,
+            scaleMode: "FILL",
+          },
+        ];
+        figma.closePlugin();
       });
-  };
-}
+    });
+};
